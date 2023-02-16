41f106f5460db08a72bdf768fdc9e5881fde1847
arkoaikins <arkoaikins@gmail.com>
2023-02-16 14:17:24 -0800

    C program that prints exactly Programming is like building a  multilingual puzzle, followed by a new line,Use the function puts,You are not allowed to use printf,Your program should end with the value 0

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+/**
+ * main -is the starting  point for the program.
+ *
+ * Return:zero if no error, one if errors
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
