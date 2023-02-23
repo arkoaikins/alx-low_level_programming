26ef1bd81c212d41861ea96a7155b8623226db34
arkoaikins <arkoaikins@gmail.com>
2023-02-23 11:48:39 -0800

    The prototypes of all my functions and the prototype of the function _putchar is in this header file called main.h

---
+++ 0x04-more_functions_nested_loops/main.h
@@ -0,0 +1,18 @@
+/* This a header file containing declarations
+ * for prototype functions defined in the 0x04-more_functions_nested_loops
+ * tasks 
+ * This is like creating my own library with declared functions
+ * These are just the header files the declaration is made by Alx in main.c
+ */
+int _putchar(char c);
+int _isupper(int c);
+int _isdigit(int c);
+int mul(int a, int b);
+void print_numbers(void);
+void print_most_numbers(void);
+void more_numbers(void);
+void print_line(int n);
+void print_diagonal(int n);
+void print_square(int size);
+void print_triangle(int size);
+void print_number(int n);
