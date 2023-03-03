74474d0ea441ce7759132f9cd4694a08384eb7fe
arkoaikins <arkoaikins@gmail.com>
2023-03-02 20:31:33 -0800

    this is the header file that contains all th prototypes of my function

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,4 +1,7 @@
-/**
+#ifndef MAIN_H
+#define MAIN_H
+
+I/**
  * This is a header file
  * It contains all the prototype of my functions
  * So this is like me creating my own library with declarations
@@ -12,3 +15,10 @@ char *_strncpy(char *dest, char *src, int n);
 int _strcmp(char *s1, char *s2);
 char *string_toupper(char *);
 char *cap_string(char *);
+char *leet(char *);
+char *rot13(char *);
+void print_number(int n);
+char *infinite_add(char *n1, char *n2, char *r, int size_r);
+void print_buffer(char *b, int size);
+
+#endif
