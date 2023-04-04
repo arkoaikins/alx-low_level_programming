abb1946feab5dc477415320e27a31e179b608d2d
arkoaikins <arkoaikins@gmail.com>
2023-04-04 16:59:39 -0700

    this is a function that prints all the elements of a listint_t list and returns the number of nodes

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,29 @@
+#include "lists.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ *print_listint - Prints all the elements of a listint_t list.
+ *@h: Pointer to the head of the list.
+ *
+ *Return: The number of nodes in the list(count).
+ */
+size_t print_listint(const listint_t *h)
+{
+	size_t count = 0;
+
+	do {
+		printf("%d\n", h->n);
+		h = h->next;
+		count++;
+	} while (h != NULL);
+	return (count);
+}
+
+/* This code solves the task 0*/
+
+/**
+ * i could use while or for loop
+ * to solve this task but i choose a do while loop
+ * just to practice more with do while loops.
+ */
