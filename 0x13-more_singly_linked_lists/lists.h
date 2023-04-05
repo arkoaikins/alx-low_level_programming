4e60ca660aedfa75ce9cf1e4546330ad509e09dc
arkoaikins <arkoaikins@gmail.com>
2023-04-04 17:55:40 -0700

    This is my header file,and it contains all the prototypes of my functions used in more singly linked lists project

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -4,7 +4,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stddef.h>
-
 /**
  * struct listint_s - singly linked list
  * @n: integer
@@ -13,7 +12,6 @@
  * Description: singly linked list node structure
  *
  */
-
 typedef struct listint_s
 {
 	int n;
