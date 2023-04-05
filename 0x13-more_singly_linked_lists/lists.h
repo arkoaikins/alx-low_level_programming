c55b51147a5c02d4ede9f0a6a4bd1e400c669254
arkoaikins <arkoaikins@gmail.com>
2023-04-04 17:20:30 -0700

    This is my header file,and it contains all the prototypes of my functions used in more singly linked lists project

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -1,9 +1,30 @@
 #ifndef LISTS_H
 #define LISTS_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stddef.h>
  /**
   * This is my header file and it contains
-  * all the protoypes of my function
+  * all the protoypes of my function.
+  *
   */
+
+
+/**
+ * struct listint_s - singly linked list
+ * @n: integer
+ * @next: points to the next node
+ *
+ * Description: singly linked list node structure
+ *
+ */
+typedef struct listint_s
+{
+    int n;
+    struct listint_s *next;
+} listint_t;
+
 
 int _putchar(char c);
 size_t print_listint(const listint_t *h);
