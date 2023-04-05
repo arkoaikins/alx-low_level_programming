96da6f81b811c65d0cd7c65bb3da95b784e8a6e4
arkoaikins <arkoaikins@gmail.com>
2023-04-04 17:31:20 -0700

    this is a function that prints all the elements of a listint_t list and returns the number of nodes

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -12,12 +12,11 @@ size_t print_listint(const listint_t *h)
 {
 	size_t counter = 0;
 
-	while (h != NULL)
-	{
-		 printf("%d\n", h->n);
-                h = h->next;
-                count++;
+	do {
+		printf("%d\n", h->n);
+		h = h->next;
+		counter++;
+	} while (h != NULL);
 
-	}
 	return (counter);
 }
