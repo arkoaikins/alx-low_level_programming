47e0b76750b9468c7bb9030e91e9742a6e5d63a0
arkoaikins <arkoaikins@gmail.com>
2023-04-04 18:52:43 -0700

    this is a function that  adds a new node at the beginning of a listint_t list,and returns the address of the new element, or NULL if it failed

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -12,22 +12,14 @@
 
 listint_t *add_nodeint(listint_t **head, const int n)
 {
+	listint_t *new_node = malloc(sizeof(listint_t));
 
-	listint_t *new_element;
-
-	if (head == NULL)
+	if (new_node == NULL)
 	{
 		return (NULL);
 	}
-	new_element = malloc(sizeof(listint_t));
-	if (new_element == NULL)
-	{
-		return (NULL);
-	}
-
 	new_node->n = n;
 	new_node->next = *head;
-	*head = new_element;
-
-	return (new_element);
+	*head = new_node;
+	return (new_node);
 }
