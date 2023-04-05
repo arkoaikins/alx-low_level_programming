bf7a279a67c277f874812c329a700b8b40ec4a5c
arkoaikins <arkoaikins@gmail.com>
2023-04-05 16:00:02 -0700

    a function that prints a listint_t linked list.

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -16,17 +16,17 @@ const listint_t **_r(const listint_t **list, size_t size, const listint_t *new)
 	const listint_t **newlist;
 	size_t i;
 
-	newlist = malloc(new_size * sizeof(listint_t *));
-	if (new_list == NULL)
+	newlist = malloc(size * sizeof(listint_t *));
+	if (newlist == NULL)
 	{
-		free(old_list);
+		free(list);
 		exit(98);
 	}
 	for (i = 0; i < size - 1; i++)
-		new_list[i] = old_list[i];
-	newlist[i] = new_node;
-	free(old_list);
-	return (new_list);
+		newlist[i] = list[i];
+	newlist[i] = new;
+	free(list);
+	return (newlist);
 }
 
 /**
@@ -38,27 +38,25 @@ const listint_t **_r(const listint_t **list, size_t size, const listint_t *new)
 
 size_t print_listint_safe(const listint_t *head)
 {
-	size_t i, num_nodes = 0;
+	size_t i, num = 0;
 	const listint_t **list = NULL;
 
 	while (head != NULL)
 	{
-		size_t i;
-
-		for (i = 0; i < num_nodes; i++)
+		for (i = 0; i < num; i++)
 		{
 			if (head == list[i])
 			{
 				printf("-> [%p] %d\n", (void *)head, head->n);
 				free(list);
-				return (num_nodes);
+				return (num);
 			}
 		}
-		num_nodes++;
-		list = _r(list, num_nodes, head);
+		num++;
+		list = _r(list, num, head);
 		printf("[%p] %d\n", (void *)head, head->n);
 		head = head->next;
 	}
 	free(list);
-	return (num_nodes);
+	return (num);
 }
