f30e815e580aeed8e0cd82c9279de25645abd55c
arkoaikins <arkoaikins@gmail.com>
2023-04-07 18:24:18 +0000

    Create C program to print string using puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,14 @@
-#include<stdio.h>
+#include <stdio.h>
+
 /**
- * main -is the starting  point for the program.
- *
- * Return:zero if no error, one if errors
+ * main - is the entry point of the program.
+ * puts: Print its argument followed by a new line
+ *        automatically,unline printf that needs \n.
+ * Return: return 0 if sucessful else returni 1.
  */
+
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
+	puts("Programming is like building a multilingual puzzle");
 	return (0);
 }
