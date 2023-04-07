52c257205f111f84702e6b509ff352faa092e25b
arkoaikins <arkoaikins@gmail.com>
2023-04-07 18:34:53 +0000

    Create C program that prints string using puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -5,10 +5,13 @@
  * puts: Print its argument followed by a new line
  *        automatically,unline printf that needs \n.
  * Return: return 0 if sucessful else returni 1.
+ * the \ in the puts is an escape character that
+ * indicates that the next double quote is not the
+ * end of the string.
  */
 
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
