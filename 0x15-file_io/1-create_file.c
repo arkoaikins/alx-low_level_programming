c9dc1ec6cdfaf3deb155e6f499dcb5d7a458a27e
arkoaikins <arkoaikins@gmail.com>
2023-04-11 11:18:58 +0000

    update a function that creates a file.

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -14,7 +14,7 @@
 
 int create_file(const char *filename, char *text_content)
 {
-	int o, w;
+	int fd, w;
 	int len = 0;
 
 	if (filename == NULL)
@@ -26,13 +26,20 @@ int create_file(const char *filename, char *text_content)
 			len++;
 	}
 
-	o = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
-	w = write(o, text_content, len);
-
-	if (o == -1 || w == -1)
+	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, S_IRUSR | S_IWUSR);
+	if (fd == -1)
 		return (-1);
 
-	close(o);
+	if (text_content != NULL)
+	{
+		w = write(fd, text_content, len);
+		if (w == -1)
+		{
+			close(fd);
+			return (-1);
+		}
+	}
 
+	close(fd);
 	return (1);
 }
