2fea14f11e7c3329d3340a3a0b2f9f4ce3ca9bfb
arkoaikins <arkoaikins@gmail.com>
2023-06-09 15:53:52 +0000

    added requirements

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -3,28 +3,29 @@
 /**
  * hash_table_create - Creates a hash table
  * @size: the size of the array
+ *
  * Return: a pointer to the newly created hash table
  */
 hash_table_t *hash_table_create(unsigned long int size)
 {
 
-	hash_table_t *hash_t;
+	hash_table_t *ha_t;
 	unsigned long int i;
 
-	hash_t = malloc(sizeof(hash_table_t));
-	if (hash_t == NULL)
+	ha_t = malloc(sizeof(hash_table_t));
+	if (ha_t == NULL)
 		return (NULL);
 
-	hash_t->size = size;
-	hash_t->array = malloc(sizeof(hash_node_t *) * size);
-	if (hash_t->array == NULL)
+	ha_t->size = size;
+	ha_t->array = malloc(sizeof(hash_node_t *) * size);
+	if (ha_t->array == NULL)
 	{
-		free(hash_t);
+		free(ha_t);
 		return (NULL);
 
 	}
 	for (i = 0; i < size; i++)
-		hash_t->array[i] = NULL;
+		ha_t->array[i] = NULL;
 
-	return (hash_t);
+	return (ha_t);
 }
