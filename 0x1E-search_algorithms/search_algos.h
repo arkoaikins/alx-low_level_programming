332b27293315ed1243f2a5a7dc58106843557a42
arkoaikins <arkoaikins@gmail.com>
2023-10-11 16:54:34 +0000

    [feature]:add data structures for task 12 and 13

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -1,6 +1,45 @@
 #ifndef SEARCH_H
 #define SEARCH_H
 
+/* Data structure for task 12 */
+
+/**
+ * struct listint_s - singly linked list
+ *
+ * @n: Integer
+ * @index: Index of the node in the list
+ * @next: Pointer to the next node
+ *
+ * Description: singly linked list node structure
+ */
+typedef struct listint_s
+{
+    int n;
+    size_t index;
+    struct listint_s *next;
+} listint_t;
+
+/* Data structure for task 13 */
+/**
+ * struct skiplist_s - Singly linked list with an express lane
+ *
+ * @n: Integer
+ * @index: Index of the node in the list
+ * @next: Pointer to the next node
+ * @express: Pointer to the next node in the express lane
+ *
+ * Description: singly linked list node structure with an express lane
+ */
+typedef struct skiplist_s
+{
+    int n;
+    size_t index;
+    struct skiplist_s *next;
+    struct skiplist_s *express;
+} skiplist_t;
+
+
+
 /* prototypes of my functions used */
 /* task 0 */
 int linear_search(int *array, size_t size, int value);
@@ -25,25 +64,5 @@ listint_t *jump_list(listint_t *list, size_t size, int value);
 
 /* task 13 */
 skiplist_t *linear_skip(skiplist_t *list, int value);
-
-/* Data structure for task 13 */
-
-/**
- * struct skiplist_s - Singly linked list with an express lane
- *
- * @n: Integer
- * @index: Index of the node in the list
- * @next: Pointer to the next node
- * @express: Pointer to the next node in the express lane
- *
- * Description: singly linked list node structure with an express lane
- */
-typedef struct skiplist_s
-{
-	int n;
-	size_t index;
-	struct skiplist_s *next;
-	struct skiplist_s *express;
-} skiplist_t;
 
 #endif
