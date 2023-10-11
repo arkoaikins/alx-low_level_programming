f24ca2b1604d55eee8ff2dd587530aa8010cbf83
arkoaikins <arkoaikins@gmail.com>
2023-10-11 16:13:32 +0000

    create header file which contains all the prototypes of your functions

---
+++ 0x1E-search_algorithms/search_algos.h
@@ -0,0 +1,49 @@
+#ifndef SEARCH_H
+#define SEARCH_H
+
+/* prototypes of my functions used */
+/* task 0 */
+int linear_search(int *array, size_t size, int value);
+
+/* task 1 */
+int binary_search(int *array, size_t size, int value);
+
+/* task 7 */
+int jump_search(int *array, size_t size, int value);
+
+/* task 9 */
+int interpolation_search(int *array, size_t size, int value);
+
+/* task 10 */
+int exponential_search(int *array, size_t size, int value);
+
+/* task 11 */
+int advanced_binary(int *array, size_t size, int value);
+
+/* task 12 */
+listint_t *jump_list(listint_t *list, size_t size, int value);
+
+/* task 13 */
+skiplist_t *linear_skip(skiplist_t *list, int value);
+
+/* Data structure for task 13 */
+
+/**
+ * struct skiplist_s - Singly linked list with an express lane
+ *
+ * @n: Integer
+ * @index: Index of the node in the list
+ * @next: Pointer to the next node
+ * @express: Pointer to the next node in the express lane
+ *
+ * Description: singly linked list node structure with an express lane
+ */
+typedef struct skiplist_s
+{
+	int n;
+	size_t index;
+	struct skiplist_s *next;
+	struct skiplist_s *express;
+} skiplist_t;
+
+#endif
